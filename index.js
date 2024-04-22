d624b9e6045f8d695bc7a24a9cc2376604043af0
allylotti <100740705+allylotti@users.noreply.github.com>
2024-04-22 15:35:20 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+async function runExample() {
+
+    var x = new Float32Array( 1, 4 )
+
+    var x = [];
+
+     x[0] = document.getElementById('box1').value;
+     x[1] = document.getElementById('box2').value;
+     x[2] = document.getElementById('box3').value;
+     x[3] = document.getElementById('box4').value;
+   
+
+    let tensorX = new onnx.Tensor(x, 'float32', [1, 4]);
+
+    let session = new onnx.InferenceSession();
+
+    await session.loadModel("./DLnet_WineData.onnx");
+    let outputMap = await session.run([tensorX]);
+    let outputData = outputMap.get('output1');
+
+   let predictions = document.getElementById('predictions');
+
+  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
+   <table>
+     <tr>
+       <td>  Real or Fake  </td>
+       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
+     </tr>
+  </table>`;
+    
+
+
+}
