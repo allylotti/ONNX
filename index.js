97d06a9f2b2d331ae069a6fee425751ea0c0661f
allylotti <100740705+allylotti@users.noreply.github.com>
2024-04-25 17:24:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 async function runExample() {
+    let x = new Float32Array(4);
 
-    var x = new Float32Array( 1, 4 )
-
-    var x = [];
-
-     x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-
+    x[0] = parseFloat(document.getElementById('box1').value);
+    x[1] = parseFloat(document.getElementById('box2').value);
+    x[2] = parseFloat(document.getElementById('box3').value);
+    x[3] = parseFloat(document.getElementById('box4').value);
 
     let tensorX = new onnx.Tensor(x, 'float32', [1, 4]);
-
     let session = new onnx.InferenceSession();
-
     await session.loadModel("./DLnet_BanknoteData.onnx");
-    let outputMap = await session.run([tensorX]);
+    
+    // Make sure 'input1' matches the expected input name of the model
+    let inputName = 'input1'; // This should be the actual input name expected by the model
+    let outputMap = await session.run({ [inputName]: tensorX });
     let outputData = outputMap.get('output1');
 
-   let predictions = document.getElementById('predictions');
-
-  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-   <table>
-     <tr>
-       <td>  Real or Fake  </td>
-       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
-     </tr>
-  </table>`;
-    
-
-
+    let predictions = document.getElementById('predictions');
+    predictions.innerHTML = `
+        <hr> Got an output tensor with values: <br/>
+        <table>
+            <tr>
+                <td>Real or Fake</td>
+                <td id="td0">${outputData.data[0].toFixed(2)}</td>
+            </tr>
+        </table>`;
 }
